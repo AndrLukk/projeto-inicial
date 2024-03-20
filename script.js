2a4d95db8497811293c7230c8300105d3f143d22
andrlukk <lucasandr17@outlook.com>
2024-03-20 07:46:43 -0300

    att

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var botao = document.getElementById('botao');
 botao = addEventListener("click", alertar);
+var saida = document.getElementById("saida-de-dados");
+var nome = document.getElementById("nome");
 
 function alertar(){
     alert("Você clicou no botão!!!!");
+    saida.innerText = "Nome: " + nome.value;
 }
